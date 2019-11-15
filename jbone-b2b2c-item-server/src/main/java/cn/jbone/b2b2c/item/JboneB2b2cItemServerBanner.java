da252cc1a50ec4878f226d04e60b9cc34163993a
马军伟 <majunwei@weidian.com>
2019-11-15 19:28:43 +0800

    初始化项目

---
+++ jbone-b2b2c-item-server/src/main/java/cn/jbone/b2b2c/item/JboneB2b2cItemServerBanner.java
@@ -0,0 +1,12 @@
+package cn.jbone.b2b2c.item;
+
+
+import cn.jbone.banner.AbastractJboneBanner;
+
+public class JboneB2b2cItemServerBanner extends AbastractJboneBanner {
+
+    @Override
+    protected String getTitle() {
+        return "Jbone B2B2C Item Server";
+    }
+}
